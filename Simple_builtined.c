7874520c0e5b2a99f3b59b5612947f49d4a5e2ca
Nje-t <njerinjesh24@gmail.com>
2023-05-25 10:18:13 -0700

    buitsin

--- Simple_builtined.c
+++ Simple_builtined.c
@@ -1,26 +1,27 @@
 #include "MY_shell.h"
+
 /**
-**handle_builtin -  finds wheter user's command is a builtin
-** @user_input: user's command
-**Return: pointer to function builtin or NULL if doesn't exists
+**handle_builtin -  It finds whether the user's command is a builtin
+** @user_input: it is the user's command
+**Return: ptr to funct builtin or NULL if doesn't exists
 **/
 int (*handle_builtin(char *user_input))()
 {
-	int counter = 0, stru_size = 0;
+	int count = 0, struct_size = 0;
 
 	builtin_struct my_builtins[] = {
 		{"exit", exiting_function},
 		{"env", envy_function},
 	};
 
-	stru_size = (sizeof(my_builtins) / sizeof(builtin_struct));
+	struct_size = (sizeof(my_builtins) / sizeof(builtin_struct));
 	if (user_input != NULL)
 	{
-		while (counter < stru_size)
+		while (count < struct_size)
 		{
-			if (comp_str(my_builtins[counter].b_name, user_input) == 0)
-				return (my_builtins[counter].b_func);
-			counter++;
+			if (comp_str(my_builtins[count].b_name, user_input) == 0)
+				return (my_builtins[count].b_func);
+			count++;
 		}
 	}
 	return (NULL);
