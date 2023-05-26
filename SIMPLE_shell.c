4f6ddd7565f0340ef08ef3c8ee57515c78c96f4a
Brian-thee-shovi <brian14198@gmail.com>
2023-05-26 05:03:12 -0700

    my shell commit

--- SIMPLE_shell.c
+++ SIMPLE_shell.c
@@ -1,22 +1,22 @@
 #include "MY_shell.h"
 /**
- * main - Entry point of Shell
- * @ac: Argument counter
- * @av: Argument values
+ * main - Entry point of Shell in my code
+ * @ac: Argument counter in my code
+ * @av: Argument values in my code
  * Return: Exit if success = 0
 */
 
 int main(int ac __attribute__((unused)), char **av __attribute__((unused)))
 {
 
-	int int_mode = 1;
-	int errcode = 0;
+	int mo_me = 1;
+	int code = 0;
 
 
-	errcode = errcode + 0;
+	code = code + 0;
 
-	int_mode = isatty(STDIN_FILENO);
-	if (int_mode == 0)
+	mo_me = isatty(STDIN_FILENO);
+	if (mo_me == 0)
 	{
 		entered_nty(av);
 	}
